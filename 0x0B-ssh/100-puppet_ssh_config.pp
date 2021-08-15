3157d3ebe8683a2cb7285b52f7f8368fd512d923
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-16 00:09:50 +0200

    Add lines in the client SSH config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,13 @@
-# set up your client SSH configuration file
+# set up your client SSH configuration file by adding lines
+
+file_line { 'Turn off passwd auth':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '    PasswordAuthentication no'
+}
+
+file_line { 'Declare identity file':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '    IdentityFile ~/.ssh/holberton'
+}
