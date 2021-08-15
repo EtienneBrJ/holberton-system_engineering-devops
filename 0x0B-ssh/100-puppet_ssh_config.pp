5e2a749cb5a47bca23f937e974512e124c2ce324
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-15 20:52:23 +0200

    Not working for me

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # set up your client SSH configuration file
 file_line { 'Declare identity file':
-  path    => '~/.ssh/config',
+  path    => '/etc/ssh/config',
   line    => '    IdentityFile ~/.ssh/holberton',
   ensure => present,
 }
 
 file_line { 'Turn off passwd auth':
-  path    => '~/.ssh/config',
+  path    => '/etc/ssh/config',
   line    => '    PasswordAuthentication no',
   ensure => present,
 }
