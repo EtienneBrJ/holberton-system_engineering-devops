d692e8f8962bda28c9439d8a0a282c62677d1363
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-17 18:40:33 +0200

    Shellcheck not gud

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,5 +2,5 @@
 
 exec {'install':
   provider => shell,
-  command  => 'apt-get -y update ; apt-get -y install nginx ; ufw allow 80 ; echo "Holberton School" > /var/www/html/index.nginx-debian.html ; sed -i "'/listen \[::\]:80 default_server;/a \\n    rewrite ^/redirect_me / permanent;'" /etc/nginx/sites-available/default ; service nginx restart'
+  command  => 'apt-get -y update ; apt-get -y install nginx ; ufw allow 80 ; echo "Holberton School" > /var/www/html/index.nginx-debian.html ; sed -i "/listen \[::\]:80 default_server;/a \\n    rewrite ^/redirect_me / permanent;" /etc/nginx/sites-available/default ; service nginx restart'
 }
