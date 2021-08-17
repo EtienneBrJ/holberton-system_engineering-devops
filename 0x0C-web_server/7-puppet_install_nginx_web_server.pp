ee355105a8d27bac65f90dbdc5f582199bdbf649
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-17 18:39:37 +0200

    Shellcheck not gud

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,6 @@
+# Installs Nginx server with redirect_me (oops)
+
+exec {'install':
+  provider => shell,
+  command  => 'apt-get -y update ; apt-get -y install nginx ; ufw allow 80 ; echo "Holberton School" > /var/www/html/index.nginx-debian.html ; sed -i "'/listen \[::\]:80 default_server;/a \\n    rewrite ^/redirect_me / permanent;'" /etc/nginx/sites-available/default ; service nginx restart'
+}
