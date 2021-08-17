16c89eebd059e9226ceace1e3e4e7c58953d704b
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-18 00:25:55 +0200

    Add advanced

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,20 @@
 # Installs Nginx server with redirect_me (oops)
 
-exec {'install':
-  provider => shell,
-  command  => 'apt-get -y update ; apt-get -y install nginx ; ufw allow 80 ; echo "Holberton School" > /var/www/html/index.nginx-debian.html ; sed -i "/listen \[::\]:80 default_server;/a \\n    rewrite ^/redirect_me / permanent;" /etc/nginx/sites-available/default ; service nginx restart'
+package {'nginx':
+  provider => present,
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School'
+}
+
+file_line { 'add redirect_me':
+  path => '/etc/nginx/sites-available/default',
+  after => 'listen 80 default server;',
+  line => 'rewrite ^/redirect_me https://github.com/EtienneBrJ permanent;,
+}
+
+exec { 'restart':
+  command => 'service nginx restart'
+  provider => 'shell',
 }
