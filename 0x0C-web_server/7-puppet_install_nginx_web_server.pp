9dc55427d85ee74bae045536d8a8178e109abfff
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-18 05:45:43 +0200

    change restart nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -9,12 +9,12 @@ file { '/var/www/html/index.html':
 
 file_line { 'add redirect_me':
   ensure => present,
-  path => '/etc/nginx/sites-available/default',
-  after => 'listen 80 default server;',
-  line => 'rewrite ^/redirect_me https://github.com/EtienneBrJ permanent;',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default server;',
+  line   => 'rewrite ^/redirect_me https://github.com/EtienneBrJ permanent;',
 }
 
 package { 'nginx':
-  ensure => running,
+  ensure  => running,
   require => Package['nginx],
 }
