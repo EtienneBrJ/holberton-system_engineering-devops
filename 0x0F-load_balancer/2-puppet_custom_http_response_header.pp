d4cad6bf344d0c6d6851239e306f8cf989f1658e
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-23 23:57:16 +0300

    Custom HTTP response header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,22 @@
+# Configure Nginx like the 2696-web-01 and add a HTTP header with the pair key: value using Puppet
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-get-update'],
+}
+
+file_line { 'b':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
