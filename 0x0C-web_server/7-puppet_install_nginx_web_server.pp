e8db496b3a747573e35cc880a115562c922d9776
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-24 00:02:58 +0300

    Fix require

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,17 +1,23 @@
 # Installs Nginx server with redirect_me
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
+}
+
 package { 'nginx':
-  ensure => installed
+  ensure  => installed,
+  require => Exec['apt-get-update'],
 }
 
 file { '/var/www/html/index.html':
   content => 'Holberton School'
 }
 
-file_line { 'add redirect_me':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default server;',
-  line   => 'rewrite ^/redirect_me https://github.com/EtienneBrJ permanent;',
+file_line { 'config':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default server;',
+  line    => 'rewrite ^/redirect_me https://github.com/EtienneBrJ permanent;',
+  require => Package['nginx'],
 }
 
 package { 'nginx':
