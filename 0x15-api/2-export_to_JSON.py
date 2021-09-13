873b8c522c6010b2dfe9e5207efa09275f3177b4
EtienneBrJ <etiennebrxv@gmail.com>
2021-09-13 16:17:11 +0200

    Fix checker

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """Script that, using this REST API, for a given employee ID
 returns information about his/her TODO list progress."""
-import requests
 import json
-from sys import argv
+import requests
+import sys
 
-
-employeeID = argv[1]
 
 if __name__ == "__main__":
+    employeeID = sys.argv[1]
     username = requests.get('https://jsonplaceholder.typicode.com/users/{}'
                             .format(employeeID)).json().get('name')
     todo = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
