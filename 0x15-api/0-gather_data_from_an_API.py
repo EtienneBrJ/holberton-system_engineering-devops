36ed2376b35729c116e115a9e403c43b72255ea0
EtienneBrJ <etiennebrxv@gmail.com>
2021-09-13 15:47:02 +0200

    Pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,27 +6,24 @@ from sys import argv
 
 
 employeeID = argv[1]
-nb_task_done = 0
-nb_task_not_done = 0
-total_tasks = 0
+task_done = 0
+task_ndone = 0
 completed_tasks = []
 
-username = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                        .format(employeeID)).json().get('name')
-todo = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
-                    .format(employeeID)).json()
+if __name__ == "__main__":
+    username = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                            .format(employeeID)).json().get('name')
+    todo = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                        .format(employeeID)).json()
 
-for obj in todo:
-    if obj['completed']:
-        nb_task_done += 1
-        completed_tasks.append(obj['title'])
-    else:
-        nb_task_not_done += 1
+    for obj in todo:
+        if obj['completed']:
+            task_done += 1
+            completed_tasks.append(obj['title'])
+        else:
+            task_ndone += 1
 
-total_tasks = nb_task_not_done + nb_task_done
+    print("Employee {} is done with tasks({}/{}):".
+          format(username, task_done, task_ndone + task_done))
 
-print("Employee {} is done with tasks({}/{}):".
-      format(username, nb_task_done, total_tasks))
-
-for task in completed_tasks:
-    print('\t {}'.format(task))
+    [print("\t {}".format(task)) for task in completed_tasks]
