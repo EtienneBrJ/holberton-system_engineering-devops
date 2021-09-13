4ca4d31f8d475af558b719122009ec36a271f5ac
EtienneBrJ <etiennebrxv@gmail.com>
2021-09-13 17:26:34 +0200

    ?

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,7 +3,7 @@
     returns information about his/her TODO list progress."""
 
 if __name__ == "__main__":
-    import json
+
     import requests
     import sys
 
@@ -11,22 +11,20 @@ if __name__ == "__main__":
     task_done = 0
     task_ndone = 0
     completed_tasks = []
-    usernameR = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                             .format(employeeID))
-    todoR = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
-                         .format(employeeID))
-
-    username = json.loads(usernameR.text)
-    todo = json.loads(todoR.text)
+    username = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                            .format(employeeID)).json().get('name')
+    todo = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                        .format(employeeID)).json()
 
     for obj in todo:
         if obj['completed']:
             task_done += 1
+
         else:
             task_ndone += 1
 
     print("Employee {} is done with tasks({}/{}):".
-          format(username['name'], task_done, task_ndone + task_done))
+          format(username, task_done, task_ndone + task_done))
 
     for task in todo:
         if task['completed']:
