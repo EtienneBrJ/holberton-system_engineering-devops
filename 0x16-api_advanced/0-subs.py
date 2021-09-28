016b73ebb939b43c1750fccc748f377e20dcc531
EtienneBrJ <etiennebrxv@gmail.com>
2021-09-28 15:38:44 +0200

    Fix else

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,5 +14,8 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:app:v0.0.0 (by /u/Any_Manager_3702)"
     }
     req = requests.get(url, headers=headers, allow_redirects=False)
-    response = req.json().get("data").get("subscribers")
-    return 0 if req.status_code == 404 else req
+    if req.status_code == 404:
+        return 0
+    else:
+        return req.json().get("data").get("subscribers")
+    
