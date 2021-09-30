49bb03ceab44adeead9b1e7e43c6df203f322491
EtienneBrJ <etiennebrxv@gmail.com>
2021-09-30 04:30:52 +0200

    FIx wp-settings

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+exec { 'settings' :                                                    
+provider => shell,                                                    
+command  => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php
+'                                                                     
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,2 @@
+Debugging WordPress #3
+Tool utilised : Strace, ps auxf
