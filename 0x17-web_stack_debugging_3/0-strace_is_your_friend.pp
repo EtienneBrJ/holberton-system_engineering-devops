81bac9e53a0edcf5e5fea71a5c6f5fced76dbd22
EtienneBrJ <etiennebrxv@gmail.com>
2021-09-30 04:34:35 +0200

    puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
-exec { 'settings' :                                                    
-provider => shell,                                                    
-command  => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php
-'                                                                     
+exec { 'settings' :
+provider => shell,
+command  => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php'
 }
