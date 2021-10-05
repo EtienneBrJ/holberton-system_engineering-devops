d493796bb4a0b4d52c4f80bfb270b89cc583e64a
EtienneBrJ <etiennebrxv@gmail.com>
2021-10-05 17:31:56 +0200

    Change soft/hqrd value

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# Set a new soft and hard value to the limit
+# of max open files for all the system
+
+exec { 'change-max-open-files-hard-limit':
+  command => "sed -i 's/holberton hard nofile 5/holberton hard nofile 1024/' /etc/security/limits.conf"
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'change-max-open-files-soft-limit':
+  command => "sed -i 's/holberton soft nofile 4/holberton soft nofile 1024/' /etc/security/limits.conf"
+  path    => '/usr/local/bin/:/bin/'
+}
