72173fadbeb8e4e73933fbf5e91557f2fbb5cdbf
EtienneBrJ <etiennebrxv@gmail.com>
2021-10-07 11:08:51 +0200

    Change value

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # Fix nginx limit request, set it to 2000 in the default file
 
 exec { 'ULIMIT':
-  command => 'sed -i "s/15/2000/" /etc/default/nginx',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-}
+} ->
 
 exec { 'restart':
   command => 'nginx restart',
